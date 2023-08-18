f26569b72f990807b287918c8152caedd1675581
Mikael Mortensen <mikaem@math.uio.no>
2023-08-18 10:18:13 +0200

    Adding answers

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,10 +2,20 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    fn = np.zeros(len(t))
+    for i, tn in enumerate(t):
+        fn[i] = f(tn)
+    return fn
+
 
 def func(t):
-    pass
+    if t >= 0 and t <= 3:
+        return np.exp(-t)
+    elif t > 3 and t <= 4:
+        return np.exp(-3*t)
+    else:
+        raise RuntimeError(f"Wrong input t = {t}")
+
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
