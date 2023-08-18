01ebd1a66908d1c2f71f386ddd1524a1f38c8b32
Mikael Mortensen <mikaem@math.uio.no>
2023-08-18 10:55:42 +0200

    Answer for differentiate.py

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,7 +2,21 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    N = len(u)
+    du = np.zeros(N)
+    du[1:-1] = (u[2:]-u[:-2])/(2*dt)
+    du[0] = (u[1]-u[0])/dt
+    du[-1] = (u[-1]-u[-2])/dt
+    return du
 
 def test_differentiate():
-    assert False
+    t = np.linspace(0, 1, 10)
+    u = t**2
+    due = 2*t
+    du = differentiate(u, t[1]-t[0])
+    assert np.linalg.norm((du-due)[1:-1], np.inf) < 1e-12
+
+if __name__ == '__main__':
+    test_differentiate()
+
+
