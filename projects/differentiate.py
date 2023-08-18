560da7f5a12b088979dd6a4307cb914601056e11
Mikael Mortensen <mikaem@math.uio.no>
2023-08-18 11:02:29 +0200

    Adding anser differentiate

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -4,6 +4,15 @@ import numpy as np
 def differentiate(u, dt):
     N = len(u)
     du = np.zeros(N)
+    du[0] = (u[1]-u[0])/dt
+    du[-1] = (u[-1]-u[-2])/dt
+    for i in range(1, N-1):
+        du[i] = (u[i+1] - u[i-1]) / (2*dt)
+    return du
+
+def differentiate_vector(u, dt):
+    N = len(u)
+    du = np.zeros(N)
     du[1:-1] = (u[2:]-u[:-2])/(2*dt)
     du[0] = (u[1]-u[0])/dt
     du[-1] = (u[-1]-u[-2])/dt
@@ -11,10 +20,11 @@ def differentiate(u, dt):
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
+    dt = t[1] - t[0]
     u = t**2
-    due = 2*t
-    du = differentiate(u, t[1]-t[0])
-    assert np.linalg.norm((du-due)[1:-1], np.inf) < 1e-12
+    du1 = differentiate(u, dt)
+    du2 = differentiate_vector(u, dt)
+    assert np.allclose(du1, du2)
 
 if __name__ == '__main__':
     test_differentiate()
