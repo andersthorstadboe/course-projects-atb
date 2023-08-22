97c909576fe46bd8da336db25bd74d5c5597ddd9
Mikael Mortensen <mikaem@math.uio.no>
2023-08-22 12:00:31 +0200

    Merging

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -18,9 +18,6 @@ def differentiate_vector(u, dt):
     du[-1] = (u[-1]-u[-2])/dt
     return du
 
-def differentiate_vector(u, dt):
-    pass
-
 def test_differentiate():
     t = np.linspace(0, 1, 10)
     dt = t[1] - t[0]
@@ -31,11 +28,3 @@ def test_differentiate():
 
 if __name__ == '__main__':
     test_differentiate()
-
-
-    t = np.linspace(0, 1, 10)
-    dt = t[1] - t[0]
-    u = t**2
-    du1 = differentiate(u, dt)
-    du2 = differentiate_vector(u, dt)
-    assert np.allclose(du1, du2)
