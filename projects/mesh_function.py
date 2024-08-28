7c5982001cf5b3ce3209067da4f865667d9a883b
anders_tb <andetb@math.uio.no>
2024-08-28 12:44:57 +0200

    Edit on tests

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -27,7 +27,7 @@ def func(t):
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
-    fun = mesh_function(func(t), t)
+    fun = mesh_function(func, t)
     assert np.allclose(fun, f)
 
 #if __name__ == '__main__':
